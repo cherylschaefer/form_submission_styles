9590759485d4207c84f1cc92f6ec506c987b0899
Cheryl Schaefer <CherylSchaefer@gmail.com>
2016-04-24 14:52:42 -0500

    added factories for remaining objects

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,5 +7,21 @@ FactoryGirl.define do
 
   factory :tribble do
     sequence(:name) { |i| "tribble#{i}" }
+    user { create(:user) }
+  end
+  
+  factory :snowflake do
+    sequence(:name) { |i| "snowflake#{i}" }
+    user { create(:user) }
+  end
+  
+  factory :putty do
+    name "Squishy Putty"
+    user { create(:user) }
+  end
+  
+  factory :diamond do
+    name "Sparkly Diamond"
+    user { create(:user) }
   end
 end
